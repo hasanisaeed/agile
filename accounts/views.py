a35b8d3cef1df3ce430bc6e1efe657b1c2f41cee
Saeed <hassanisaeed19@yahoo.com>
2021-10-15 16:17:53 +0330

    feat: profile card

--- accounts/views.py
+++ accounts/views.py
@@ -9,7 +9,11 @@ class SignUpView(generic.CreateView):
     template_name = 'registration/signup.html'
 
 
-class DashboardView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'dashboard/index.html'
+class DashboardView(generic.TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        from django.contrib.auth.models import User
+        context['users'] = User.objects.all()
+        return context
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
+
+from accounts.views import DashboardView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', DashboardView.as_view(), name='home'),
 ]
