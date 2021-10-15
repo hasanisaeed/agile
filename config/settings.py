9d37a58c653a9578b6eeeab91218ec7016e9e30e
Saeed <hassanisaeed19@yahoo.com>
2021-10-15 22:26:49 +0330

    fix: timezone change to asia/tehran

--- config/settings.py
+++ config/settings.py
@@ -100,7 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
+LANGUAGE_CODE = 'fa-IR'
 
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
