95afdd11e2f6f426c48fe2f48ac83cbe7cc49311
Saeed <hassanisaeed19@yahoo.com>
2021-10-17 22:29:40 +0330

    fix: default database changed

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 import dj_database_url
 
