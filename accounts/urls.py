56b5e17158a0eab973a707b912820fce958715bf
Saeed <hassanisaeed19@yahoo.com>
2021-12-22 07:45:27 +0330

    feat: performance charts

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
-from .views import DashboardView, signup_view, apply_attendance
-
-# from .views import SignUpView, DashboardView
-
+from .views import DashboardView, signup_view, apply_attendance, line_chart, line_chart_json
 
 urlpatterns = [
     path('signup/', signup_view, name='signup'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('attendance/<int:pk>', apply_attendance, name='attendance'),
+    path('chart', line_chart, name='line_chart'),
+    path('chartJSON', line_chart_json, name='line_chart_json'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -110,3 +110,28 @@ def attendance_helper(user: User):
     enter_time = attendance.enter.strftime('%H:%M:%S')
     exit_time = '' # TODO attendance.exit.strftime('%H:%M:%S')
     return enter_time, exit_time, attendance.id
+
+
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        """Return 7 labels for the x-axis."""
+        return ["January", "February", "March", "April", "May", "June", "July"]
+
+    def get_providers(self):
+        """Return names of datasets."""
+        return ["Central", "Eastside", "Westside"]
+
+    def get_data(self):
+        """Return 3 datasets to plot."""
+
+        return [[75, 44, 92, 11, 44, 95, 35],
+                [41, 92, 18, 3, 73, 87, 92],
+                [87, 21, 94, 3, 90, 13, 65]]
+
+
+line_chart = TemplateView.as_view(template_name='performance.html')
+line_chart_json = LineChartJSONView.as_view()
