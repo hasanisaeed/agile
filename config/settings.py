61083798ec5f4f19725c8921df6a016fcad79e69
Saeed <hassanisaeed19@yahoo.com>
2021-12-24 11:22:05 +0330

    refactor: using `UTC` timezone

--- config/settings.py
+++ config/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'fa-IR'
+# LANGUAGE_CODE = 'fa-IR'
 
-TIME_ZONE = 'Asia/Tehran'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
