5f13b265f3458d1f7b05c34e68089d6bb32fd160
Saeed <hassanisaeed19@yahoo.com>
2021-12-24 11:30:23 +0330

    chore: remove unused file

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
 from utils.dataset_model import ChartModelView
-from .views import DashboardView, signup_view, apply_attendance
+from .views import DashboardView, apply_attendance
 
 urlpatterns = [
-    # path('signup/', signup_view, name='signup'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('attendance/<int:pk>', apply_attendance, name='attendance'),
     path('chart', ChartModelView.as_view(), name='line_chart'),
