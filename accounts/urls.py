9f1454d44215612f8fd807c87dfebaf372895c07
Saeed <hassanisaeed19@yahoo.com>
2021-12-24 08:16:03 +0330

    fix: remove unused url

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from utils.dataset_model import ChartModelView
 from .views import DashboardView, signup_view, apply_attendance
 
 urlpatterns = [
-    path('signup/', signup_view, name='signup'),
+    # path('signup/', signup_view, name='signup'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('attendance/<int:pk>', apply_attendance, name='attendance'),
     path('chart', ChartModelView.as_view(), name='line_chart'),
--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,6 @@ from accounts.views import DashboardView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/', include('django.contrib.auth.urls')),
     path('', DashboardView.as_view(), name='home'),
 ]
