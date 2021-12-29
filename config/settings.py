054e16a415453bed639294445eaa0c8e72bdd2f9
Saeed Hasani Borzadaran <hassanisaeed19@yahoo.com>
2021-12-29 21:29:42 +0330

    fix: debug mode set false

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = P_SECRET_KEY
 AUTH_USER_MODEL = 'accounts.CustomUser'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
